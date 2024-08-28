196027ca2be7a1588175ab1414d5160087682fa7
Michell Castillo <webdev.michell@gmail.com>
2024-08-28 10:41:19 -0400

    feat: change api properties

--- src/app/ai-chat/components/welcome-view/welcome-view.component.ts
+++ src/app/ai-chat/components/welcome-view/welcome-view.component.ts
@@ -3,30 +3,29 @@ import { Component, OnInit } from '@angular/core';
 import { ConfService } from 'src/app/conf/conf.service';
 
 type WelcomeView = {
-  title: string,
-  subtitle: string,
-  description: string,
-  subtitle2?: string,
-  description2?: string
-}
+  title: string;
+  subtitle: string;
+  description: string;
+  subtitle2?: string;
+  description2?: string;
+};
 
 @Component({
   selector: 'app-welcome-view',
   templateUrl: './welcome-view.component.html',
   standalone: true,
   styleUrls: ['./welcome-view.component.scss'],
-  imports: [CommonModule, JsonPipe]
+  imports: [CommonModule, JsonPipe],
 })
-export class WelcomeViewComponent implements OnInit{
-
-  conf!: WelcomeView 
-  constructor(public confService: ConfService){
-
-  }
+export class WelcomeViewComponent implements OnInit {
+  conf: WelcomeView | undefined = undefined;
+  constructor(public confService: ConfService) {}
 
   ngOnInit(): void {
-   this.confService.conf.subscribe(conf => {
-    this.conf = conf
-   })
+    this.confService.conf.subscribe((conf) => {
+     if(conf?.title){
+      this.conf = conf;
+     }
+    });
   }
 }
--- src/app/ai-components-layout/ai-components-layout.component.ts
+++ src/app/ai-components-layout/ai-components-layout.component.ts
@@ -62,13 +62,13 @@ export class AiComponentsLayoutComponent implements OnInit, OnChanges {
   ngOnInit(): void {
     if (this.room) {
       this.getConf();
-    } else if(this.config) {
+    } else if (this.config) {
       this.getAttributeConf();
     }
 
-    console.log(this.conf);
+    //console.log(this.conf);
 
-    console.log(this.confService.conf.value);
+    // console.log(this.confService.conf.value);
 
     const savedShowDefaultComponent = localStorage.getItem('componentToShow');
 
@@ -89,34 +89,33 @@ export class AiComponentsLayoutComponent implements OnInit, OnChanges {
       // Convertir el string JSON a un objeto
       const decodedJson = JSON.parse(jsonString);
 
-      console.log(decodedJson); // Verifica el resultado en la consola
+     //  console.log(decodedJson); // Verifica el resultado en la consola
 
       this.dataTransform(decodedJson.aiConfig.css);
       this.conf = decodedJson.aiConfig.welcomeView;
       this.confService.conf.next({
         ...decodedJson.aiConfig.welcomeView,
       });
-      console.log(this.styles);
-
+      // console.log(this.styles);
     } catch (error) {
       console.error('Error al manejar el input base64', error);
     }
   }
 
   getConf() {
-    this.http.get(`./assets/ai.config.json`).subscribe(
-      (conf: any) => {
-        this.dataTransform(conf.aiConfig.css);
-        this.conf = conf.aiConfig.welcomeView;
-        this.confService.conf.next({
-          ...conf.aiConfig.welcomeView,
-        });
-        console.log(this.styles);
+    this.http.get(`https://api.ubiqq.com/live/1.1/room/ubiqq`).subscribe({
+      next: (conf: any) => {
+        if (conf.aiConfig) {
+          this.dataTransform(conf.aiConfig.css);
+          this.conf = conf.aiConfig.welcomeView;
+          this.confService.conf.next({
+            ...conf.aiConfig.welcomeView,
+          });
+          // console.log(this.styles);
+        }
       },
-      (error) => {
-        console.error('Error:', error);
-      }
-    );
+      error: (err) => console.log(err),
+    });
   }
 
   dataTransform(css: any) {
@@ -124,53 +123,42 @@ export class AiComponentsLayoutComponent implements OnInit, OnChanges {
       '--header-bg': css?.header?.background ?? '',
       '--header-color': css?.header?.color ?? '',
       '--header-active-color': css?.header?.['active-color'] ?? '',
-      '--border-color': css?.general?.borders?.['border-color'] ?? '',
-      '--border-width': css?.general?.borders?.['border-width'] ?? '',
-      '--border-style': css?.general?.borders?.['border-style'] ?? '',
-      '--radius': css?.general?.borders?.['border-radius'] ?? '',
-      '--container-bg': css?.general?.background ?? '',
-      '--container-color': css?.general?.color ?? '',
+      '--border-color': css?.borders?.['border-color'] ?? '',
+      '--border-width': css?.borders?.['border-width'] ?? '',
+      '--border-style': css?.borders?.['border-style'] ?? '',
+      '--radius': css?.borders?.['border-radius'] ?? '',
+      '--container-bg': css?.container?.background ?? '',
+      '--container-color': css?.container?.color ?? '',
       '--menu-bg': css?.sidebarAndMenu?.background ?? '',
       '--menu-color': css?.sidebarAndMenu?.color ?? '',
-      '--user-message-bg': css?.chat?.['userMessage']?.background ?? '',
-      '--user-message-color': css?.chat?.['userMessage']?.color ?? '',
-      '--font-size-base': css?.general?.font?.['font-size'] ?? '',
-      '--font-normal': css?.general?.font?.['font-weight'] ?? '',  // Nota: Cambié 'font-wieght' a 'font-weight'
-      '--font-bold': css?.general?.font?.['font-weight'] ?? '',
-      '--input-bg': css?.general?.inputs?.background ?? '',
-      '--input-color': css?.general?.inputs?.color ?? '',
-      '--primary': css?.general?.['primary-color'] ?? '',
-      '--box-shadow': css?.general?.['box-shadow'] ?? '',
-      '--primary-btn-bg': css?.general?.['primary-buttons']?.background ?? '',
-      '--primary-btn-color': css?.general?.['primary-buttons']?.color ?? '',
-      '--secondary-btn-bg': css?.general?.['secondary-buttons']?.background ?? '',
-      '--secondary-btn-color': css?.general?.['secondary-buttons']?.color ?? '',
-      '--state-bg': css?.general?.['activeAndHoverElements']?.background ?? '',
-      '--state-color': css?.general?.['activeAndHoverElements']?.color ?? '',
-      '--warning-btn-bg': css?.confirmationModal?.['confirm-button']?.background ?? '',
-      '--warning-btn-color': css?.confirmationModal?.['confirm-button']?.color ?? '',
+      '--user-message-bg': css?.["userMessage"]?.background ?? '',
+      '--user-message-color': css?.["userMessage"]?.color ?? '',
+      '--font-size-base': css?.font?.['font-size'] ?? '',
+      '--font-normal': css?.font?.['font-weight'] ?? '',
+      '--font-bold': css?.font?.['font-weight'] ?? '',
+      '--input-bg': css?.inputs?.background ?? '',
+      '--input-color': css?.inputs?.color ?? '',
+      '--primary': css?.title?.['color'] ?? '',
+      '--box-shadow': css?.container?.['box-shadow'] ?? '',
+      '--primary-btn-bg': css?.['primary-buttons']?.background ?? '',
+      '--primary-btn-color': css?.['primary-buttons']?.color ?? '',
+      '--secondary-btn-bg':
+        css?.['secondary-buttons']?.background ?? '',
+      '--secondary-btn-color': css?.['secondary-buttons']?.color ?? '',
+      '--state-bg': css?.['activeAndHoverElements']?.background ?? '',
+      '--state-color': css?.['activeAndHoverElements']?.color ?? '',
+      '--warning-btn-bg':
+        css?.confirmModalButton?.background ?? '',
+      '--warning-btn-color':
+        css?.confirmModalButton?.color ?? '',
     };
-    
   }
 
-  /**
-   * 
-   * getConf() {
-    this.http
-      .get(`https://api.ubiqq.com/live/1.1/room/ubiqq`)
-      .subscribe((conf) => {
-        console.log(conf);
-      }, (error) => {
-        console.error('Error:', error);
-      });
-  }
-   */
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['config'] && changes['config'].currentValue) {
       this.getAttributeConf();
     }
   }
-  
 
   showSelectedModule(module: any) {
     this.componentToShow.component = module.key;
@@ -186,9 +174,6 @@ export class AiComponentsLayoutComponent implements OnInit, OnChanges {
       this.isShowMobileMenu = false;
     }
   }
-
-
-
 
   checkIfMobile() {
     return window.innerWidth <= 1000;
